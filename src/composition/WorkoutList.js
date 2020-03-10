30c205d5ee53919b3482ac18b3cfd2f75ec4600b
jennifrmarie <jen@EPICACXIV.attlocal.net>
2020-03-10 08:56:30 -0400

    commit changes for heroku

--- src/composition/WorkoutList.js
+++ src/composition/WorkoutList.js
@@ -33,7 +33,7 @@ export default class WorkoutList extends Component {
             {item.weight} lbs
           </div>
           <Link to={'/edit-workout/' + item.id} tag='button'>edit</Link>
-          <Link to={'/edit-workout'} className="remove-button" onClick={e => this.context.removeItem(item.id)}> {'    '}remove </button>
+          <Link to={'/add-workout'} className="remove-button" onClick={e => this.context.removeItem(item.id)}> {'    '}remove </Link>
 
             </li>)
           }
